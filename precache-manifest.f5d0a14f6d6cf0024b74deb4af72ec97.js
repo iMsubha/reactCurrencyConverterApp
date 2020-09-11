12f00709668a29f52bd9a041d961b39d4d4aa484
Subha Fairuz <iMsubha>
2020-09-12 01:46:22 +0600

    Updates

---
+++ precache-manifest.f5d0a14f6d6cf0024b74deb4af72ec97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2ef7e345e7bfc434d50f4fa5ebc5ebb",
+    "url": "/reactCurrencyConverterApp/index.html"
+  },
+  {
+    "revision": "0179a87e4fac1987fc53",
+    "url": "/reactCurrencyConverterApp/static/css/main.ea465f4e.chunk.css"
+  },
+  {
+    "revision": "102b15d6804e3c1959de",
+    "url": "/reactCurrencyConverterApp/static/js/2.c80eae17.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactCurrencyConverterApp/static/js/2.c80eae17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0179a87e4fac1987fc53",
+    "url": "/reactCurrencyConverterApp/static/js/main.d1f157d3.chunk.js"
+  },
+  {
+    "revision": "648a21463a7da22e82c4",
+    "url": "/reactCurrencyConverterApp/static/js/runtime-main.f8a5d073.js"
+  }
+]);
